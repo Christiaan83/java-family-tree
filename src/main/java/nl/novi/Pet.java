eae7cd9b55dd8289dd73e1647020ef5f1f3926e4
Christiaan83 <c.zielman@gmail.com>
2024-01-08 12:36:00 +0100

    alle gevraagde methodes aangemaakt

--- src/main/java/nl/novi/Pet.java
+++ src/main/java/nl/novi/Pet.java
@@ -37,4 +37,14 @@ public class Pet {
     public void setSpecies(String species) {
         this.species = species;
     }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+
 }
